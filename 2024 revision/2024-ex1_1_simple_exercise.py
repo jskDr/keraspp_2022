9c65ee9dc54cb5cf145dbc2edc727e388f4154ba
jskdr <jamessungjin.kim@gmail.com>
2024-04-29 16:42:31 -0700

    tensorflow

---
+++ 2024 revision/2024-ex1_1_simple_exercise.py
@@ -0,0 +1,24 @@
+# 1. 케라스 패키지 임포트
+import keras 
+import numpy
+
+# 2. 데이터 지정
+x = numpy.array([0, 1, 2, 3, 4]) 
+y = x * 2 + 1
+
+# 3. 인공신경망 모델링 
+from keras import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(1,input_shape=(1,)))
+model.compile("SGD", "mse")
+
+# 4. 주어진 데이터로 모델 학습
+model.fit(x[:2], y[:2], epochs=1000, verbose=0)
+
+# 5.성능 평가
+y_pred = model.predict(x[2:]).flatten()
+print('Targets:', y[2:])
+print('Predictions:', y_pred)
+print('Errors:', y[2:] - y_pred)
---
+++ tensorflow/basic_00.py
@@ -0,0 +1,9 @@
+"""
+Basic TensorFlow example 00
+"""
+import tensorflow as tf
+
+x = tf.constant([[1., 2., 3.],
+                 [4., 5., 6.]])
+
+print(x)
